b93e7fae255a38762c9ffe6786c07d94ef701230
lzabb <lo.zabban@gmail.com>
2018-01-03 20:27:50 +0100

    Update tinyblockchain.py

--- tinyblockchain.py
+++ tinyblockchain.py
@@ -6,31 +6,10 @@ Created on Thu Dec 28 19:22:05 2017
 @author: lorenzozabban
 """
 
-import hashlib as hasher
 import datetime as date
 import numpy as np
 from CheckBalance import BalanceCheck
-
-
-
-class Block(object):
-    
-    
-    def __init__(self, index, timestamp, data, previous_hash):
-        self.index = index
-        self.timestamp = timestamp
-        self.data = data
-        self.previous_hash = previous_hash
-        self.hash = self.hash_block()
-            
-    
-    def hash_block(self):
-        sha = hasher.sha256(str(self.index) +
-                            str(self.timestamp) + 
-                            str(self.data) + 
-                            str(self.previous_hash))
-        return sha.hexdigest()
- 
+from Blocks import Block
 
 
 def create_genesis_block():
@@ -44,8 +23,6 @@ def next_block(last_block, list_transaction):
     this_hash = last_block.hash
     return Block(this_index, this_timestamp, this_data, this_hash)
 
-
-###############################################################################
 
 
 blockchain = [create_genesis_block()]
