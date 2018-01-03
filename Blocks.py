4919670058cb2ba28f9a14b314cfd1c9101d4405
lzabb <lo.zabban@gmail.com>
2018-01-03 20:24:42 +0100

    Add files via upload

---
+++ Blocks.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  3 20:21:03 2018
+
+@author: lorenzozabban
+"""
+
+import hashlib as hasher
+
+class Block(object):
+    
+    
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.hash_block()
+            
+    
+    def hash_block(self):
+        sha = hasher.sha256(str(self.index) +
+                            str(self.timestamp) + 
+                            str(self.data) + 
+                            str(self.previous_hash))
+        return sha.hexdigest()
