7adfcf6ac2a4a80643d4bf026981d491522e4ea4
cheerfulballs <34776890+cheerfulballs@users.noreply.github.com>
2018-01-05 16:11:00 +0000

    Update chain_engine.py

--- chain_engine.py
+++ chain_engine.py
@@ -27,9 +27,7 @@ class Engine(object):
       
     def gen_wallet(self):
         
-        wallets_keys = []
-        for i in np.arange(10): # set how many wallets
-            wallets_keys.append(Wallet().public_key)
+        wallets_keys = [Wallet().public_key for _ in range(10)]
         wallets_keys.append('toni')
         return wallets_keys
 
@@ -52,5 +50,54 @@ class Engine(object):
             return transaction
         else:
             return status
-    
+        
+
+
+class Round_(object):
          
+    def extract_name(self,list_of_dictionaries, name):
+            name_list_loose = [list_of_dictionaries[d][name] for d in range(0,len(list_of_dictionaries))]
+            print name_list_loose
+            name_list = [name_list_loose[0]]
+            for name_ in name_list_loose:
+                if name_list.count(name_) >= 1:
+                    pass
+                else:
+                    name_list.append(name_)
+            return name_list
+        
+    def round_(self, transactions, blockchain):
+        print ''' 
+                Round for new block begins
+                '''
+        accepted_transactions = [] 
+        # need to isolate all the from accounts to see if they have the balance to do THE POSSIBLY MANY transactions they asked
+        from_list = self.extract_name(transactions, 'from')
+        print 'Senders list:', from_list,'.'
+        for from_i in from_list:
+            amount_i = []
+            for t in transactions:     #some repeated operations
+                if from_i == t['from']:
+                    amount_i.append(t['amount'])
+                else:
+                    pass
+            print 'All amounts requested to transfer by', from_i,':', amount_i,'.', 'Total:', sum(amount_i),'.'
+            status = Engine().payment(blockchain, from_i, 'N/A', sum(amount_i)) 
+            if status == 'insufficient funds':
+                print status
+            else:
+                for t in transactions:
+                    if from_i == t['from']:
+                        accepted_transactions.append({'from' : t['from'], 'to' : t['to'], 'amount' : t['amount']})
+                    else:
+                        pass
+            
+        if accepted_transactions != []:
+            previous_block = blockchain[-1]
+            block2add = Engine().next_block(previous_block, accepted_transactions)
+            blockchain.append(block2add)
+            previous_block = block2add
+            print 'Block #', block2add.index,' Hash:', block2add.hash,'.'
+        else:
+            pass
+  
